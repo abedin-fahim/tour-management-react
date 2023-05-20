cd61e727b88de1b78dbd8a8cd8179dd35df43327
Joynal Abedin <cfcfahim26@gmail.com>
2023-05-20 22:48:54 +0600

    finish version

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,43 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, name, image, info, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const readMoreHandler = () => {
+    setReadMore(!readMore);
+  };
+  const notInterestedHandler = () => {
+    removeTour(id);
+  };
+
+  return (
+    <article className='single-tour'>
+      <img
+        src={image}
+        alt={name}
+      />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <div>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+            <button onClick={readMoreHandler}>
+              {readMore ? 'Show less' : 'Read more'}
+            </button>
+          </p>
+          <button
+            className='delete-btn'
+            onClick={notInterestedHandler}
+          >
+            Not interested
+          </button>
+        </div>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <h2 className='title'>View all the available tours</h2>
+      <div className='underline'></div>
+      <div>
+        {tours.map((tour) => {
+          return (
+            <Tour
+              key={tour.id}
+              {...tour}
+              removeTour={removeTour}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
